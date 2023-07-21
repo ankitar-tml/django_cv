343dee84d5cf56471c096fd4079c1472be597660
kp926149 <kp926149.ttl@tatamotors.com>
2023-07-21 12:01:09 +0530

    removed vin traceability search api

--- vin_details/urls.py
+++ vin_details/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
 		path("mes/vin_details/plant=<str:plant>&status=<str:status_name>&from_date=<str:from_date>",views.vin_details,name="vin_details"),
 		#path("mes/vin_details/plant=<str:plant>&from_date=<str:from_date>",views.vin_details,name="vin_details")
 		#path("api/vin_details",views.vin_details,name="vin_details")
-		path("mes/vin_details/part_vin_traceability_search_by_srno",views.vin_traceability_search_by_serial_no,name="vin_details"),
-		path("mes/vin_details/part_vin_traceability_search_by_srno_download",views.vin_traceability_search_by_serial_no_download,name="vin_details")
                 ]
 
